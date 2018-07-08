05a3b6bd7f70dca15fb2499fe45e759a212f5ef8
a-meng <304376629@qq.com>
2018-07-08 20:33:33 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
-module.exports={
+module.exports = {
+    baseUrl: '',
     pages: {
         index: 'src/index/main.js',
-        test1:'src/test1/main.js',
-        test2:'src/test2/main.js',
-        test3:'src/test3/main.js'
-      }
+        test1: 'src/test1/main.js',
+        test2: 'src/test2/main.js',
+        test3: 'src/test3/main.js'
+    }
 }
